8078c96ec4f41acaac2f0ecbcf7235a5f77986f2
Camposm97 <camposm1205@gmail.com>
2019-11-25 08:38:45 -0500

    Created School.java and PasswordUtil.java

---
+++ app/src/main/java/model/School.java
@@ -0,0 +1,13 @@
+package model;
+
+public class School {
+    private int id;
+    private int regionId;
+    private String name;
+    private String city;
+    private String state;
+    private String zip;
+    private String url;
+    private int studentSize;
+
+}
