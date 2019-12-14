a5cbcf5dc9d79cc53eab797bf564e118327a9ee6
Camposm97 <camposm1205@gmail.com>
2019-12-14 17:52:48 -0500

    Implementing SignUp Button

--- app/src/main/java/com/campos/app/SignUpActivity.java
+++ app/src/main/java/com/campos/app/SignUpActivity.java
@@ -3,6 +3,9 @@ package com.campos.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.campos.R;
 
@@ -12,5 +15,16 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        initListeners();
+    }
+
+    private void initListeners() {
+        Button btSignUp = findViewById(R.id.signup_btSignup);
+        btSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.println(Log.ASSERT, "0", "Users attempting to sign up!");
+            }
+        });
     }
 }
